6a376ae4fdb2d6084e3992e0ee8a125683af6998
Салахеев Данис Фанисович <DFSalakheev@sberbank.ru>
2022-05-09 23:14:01 +0300

    поменял реализацию проверки open в StoppableAuction

--- src/main/java/com/fillthegaps/study/salakheev/exam_1/StoppableAuction.java
+++ src/main/java/com/fillthegaps/study/salakheev/exam_1/StoppableAuction.java
@@ -1,26 +1,39 @@
 package com.fillthegaps.study.salakheev.exam_1;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 /**
- * @version 1.3
+ * @version 1.4
  */
-public class StoppableAuction extends OptimisticAuction implements Proposing<OptimisticAuction.Bid> {
+public class StoppableAuction implements Proposing<OptimisticAuction.Bid> {
 
     private volatile boolean open;
+    private final OptimisticAuction.Notifier notifier;
+    private final AtomicReference<OptimisticAuction.Bid> atomicBidRef;
 
     public StoppableAuction() {
         this.open = true;
+        this.notifier = new OptimisticAuction.Notifier();
+        this.atomicBidRef = new AtomicReference<>(new OptimisticAuction.Bid(null, null, 0L));
     }
 
     @Override
-    public boolean propose(Bid newBid) {
-        return isOpen() && super.propose(newBid);
+    public boolean propose(OptimisticAuction.Bid newBid) {
+        OptimisticAuction.Bid old = atomicBidRef.get();
+        if (!open
+                && getLatestBid().price > newBid.price
+                && !atomicBidRef.compareAndSet(old, newBid)) {
+            return false;
+        }
+        notifier.sendOutdatedMessage(old);
+        return true;
     }
 
-    public synchronized void stopAuction() {
+    public OptimisticAuction.Bid getLatestBid() {
+        return atomicBidRef.get();
+    }
+
+    public void stopAuction() {
         this.open = false;
-    }
-
-    private synchronized boolean isOpen() {
-        return this.open;
     }
 }
