d92588782751b3fe2014f9f457ce68ade3caebf4
Салахеев Данис Фанисович <DFSalakheev@sberbank.ru>
2022-05-20 01:17:34 +0300

    новая реализация OrderService с неизменяемыми переменными

--- src/main/java/com/fillthegaps/study/salakheev/immutable/Order.java
+++ src/main/java/com/fillthegaps/study/salakheev/immutable/Order.java
@@ -16,15 +16,35 @@ public class Order {
     private static final LongAdder nextId = new LongAdder();
     private final Long id;
     private final List<Item> items;
-    private volatile PaymentInfo paymentInfo;
-    private volatile boolean isPacked;
-    private volatile Status status;
+    private PaymentInfo paymentInfo;
+    private boolean isPacked;
+    private Status status;
 
     public Order(List<Item> items) {
         this.items = items;
         this.status = Status.CREATED;
         nextId.increment();
         this.id = nextId.longValue();
+    }
+
+    public Order(Long id, List<Item> items, PaymentInfo paymentInfo, boolean isPacked, Status status) {
+        this.id = id;
+        this.items = items;
+        this.paymentInfo = paymentInfo;
+        this.isPacked = isPacked;
+        this.status = status;
+    }
+
+    public Order withStatus(Status status) {
+        return new Order(this.id, this.items, this.paymentInfo, this.isPacked, status);
+    }
+
+    public Order withPaymentInfo(PaymentInfo paymentInfo) {
+        return new Order(this.id, this.items, paymentInfo, this.isPacked, this.status);
+    }
+
+    public Order withPacked() {
+        return new Order(this.id, this.items, this.paymentInfo, true, this.status);
     }
 
     public boolean checkStatus() {
--- src/main/java/com/fillthegaps/study/salakheev/immutable/OrderService.java
+++ src/main/java/com/fillthegaps/study/salakheev/immutable/OrderService.java
@@ -1,7 +1,11 @@
 package com.fillthegaps.study.salakheev.immutable;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import static java.util.concurrent.CompletableFuture.runAsync;
 
 public class OrderService {
 
@@ -14,22 +18,37 @@ public class OrderService {
     }
 
     public void updatePaymentInfo(long cartId, PaymentInfo paymentInfo) {
-        final Order order = currentOrders.get(cartId);
-        order.setPaymentInfo(paymentInfo);
-        if (order.checkStatus()) {
-            deliver(order);
-            order.setStatus(Status.DELIVERED);
-        }
+        currentOrders
+                .computeIfPresent(cartId, (c, o) -> {
+                    Order paidOrder = o.withPaymentInfo(paymentInfo);
+                    currentOrders.put(cartId, o.withPaymentInfo(paymentInfo));
+                    if (paidOrder.checkStatus()) {
+                        runAsync(() -> deliver(paidOrder));
+                        Order delivered = paidOrder.withStatus(Status.DELIVERED);
+                        currentOrders.put(delivered.getId(), delivered);
+                    }
+                    System.out.println("");
+                    return paidOrder;
+                });
     }
 
     public void setPacked(long cartId) {
-        final Order order = currentOrders.get(cartId);
-        order.setPacked(true);
-        if (order.checkStatus()) {
-            deliver(order);
-        }
+        currentOrders
+                .computeIfPresent(cartId, (c, o) -> {
+                    final Order packedOrder = o.withPacked();
+                    currentOrders.put(cartId, packedOrder);
+                    if (packedOrder.checkStatus()) {
+                        runAsync(() -> deliver(packedOrder));
+                    }
+                    return packedOrder;
+                });
     }
 
     private void deliver(Order order) {
+        System.out.println("OK!");
+    }
+
+    public Map<Long, Order> getOrders() {
+        return Collections.unmodifiableMap(currentOrders);
     }
 }
